2222d989911fdc54276283f2431da89858ea4cff
shreyatripathi372 <tripathishreya550@gmail.com>
2026-07-23 10:41:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;        // nums1 ke last valid element ka index
-        int j = n - 1;        // nums2 ke last element ka index
-        int k = m + n - 1;    // nums1 ke last position (total size)
 
-        // Jab tak dono arrays mein elements bache hain
+        int i = m - 1;          
+        int j = n - 1;         
+        int k = m + n - 1;      
+
         while (i >= 0 && j >= 0) {
+
             if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];  // bada element nums1 se lo
-            } else {
-                nums1[k--] = nums2[j--];  // bada element nums2 se lo
+                nums1[k] = nums1[i];
+                i--;
             }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
         }
 
-        // Agar nums2 mein elements bache hain
+
         while (j >= 0) {
-            nums1[k--] = nums2[j--];
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
