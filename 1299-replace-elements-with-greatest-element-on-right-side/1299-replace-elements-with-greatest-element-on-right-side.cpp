41d615a09ab3375c55e09ba909f24cbe57a206fa
shreyatripathi372 <tripathishreya550@gmail.com>
2026-09-19 00:45:32 +0530

    Time: 0 ms (100%), Space: 72.7 MB (44.97%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    vector<int> replaceElements(vector<int>& arr) { 
+    vector<int> replaceElements(vector<int>& arr) {
         int n = arr.size();
-        vector<int> ans(n); 
-        ans[n-1]=-1;  
-        int temp=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            ans[i]=temp;
-            if(arr[i]>temp){
-                temp=arr[i];
+        vector<int> ans(n); // [0,0,0,0,0,0]
+        ans[n - 1] = -1;
+        // ans=[0,0,0,0,-1] -> ye kvv karayegi? tho shi kar
+        int temp = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            ans[i] = temp;
+            if (arr[i] > temp) {
+                temp = arr[i];
             }
         }
         return ans;
